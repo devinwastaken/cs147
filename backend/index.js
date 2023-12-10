a50cb86b3ebb72b71ad09fc703713cfd5e324047
devinwastaken <83556128+devinwastaken@users.noreply.github.com>
2023-12-09 23:52:56 -0800

    setup

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const fs = require('fs')
+
+const tempLogger = fs.createWriteStream('data/temp.csv', {
+    flags: 'a'
+})
+
+const lightLogger = fs.createWriteStream('data/light.csv', {
+    flags: 'a'
+})
+
+app.post('/', (req, res) => {
+    const temp = req.header('temperature')
+    const light = req.header('light')
+    console.log(`~[${Date.now()}] temp: ${temp}c, light: ${light}`)
+    tempLogger.write(`${temp},${Date.now()}\n`);
+    lightLogger.write(`${light},${Date.now()}\n`);
+})
+
+app.use('/data', express.static('data'))
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
