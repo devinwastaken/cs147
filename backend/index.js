536a43b523ef1132bc53b5bf355da4e912f7250c
devinwastaken <83556128+devinwastaken@users.noreply.github.com>
2023-12-10 00:02:57 -0800

    Add post response

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,9 @@ app.post('/', (req, res) => {
     const temp = req.header('temperature')
     const light = req.header('light')
     console.log(`~[${Date.now()}] temp: ${temp}c, light: ${light}`)
-    tempLogger.write(`${temp},${Date.now()}\n`);
-    lightLogger.write(`${light},${Date.now()}\n`);
+    tempLogger.write(`${temp},${Date.now()}\n`)
+    lightLogger.write(`${light},${Date.now()}\n`)
+    res.send('Got a POST request')
 })
 
 app.use('/data', express.static('data'))
